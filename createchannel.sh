51db4dbeb119caa7afcf678aa7aa1403dfbf2a71
jyg007 <jeanyves.girard@fr.ibm.com>
2018-02-10 17:26:40 +0000

    modif cosmetique pour le script pour rejoindre le channel

--- createchannel.sh
+++ createchannel.sh
@@ -1,3 +1,11 @@
+echo
+echo "Creating Channel"
 CHANNEL_NAME="labchannel" create/create_channel.sh
+
+echo
+echo "Peer1 Org1 joining the channel"
 CHANNEL_NAME="labchannel" PEER_MSPID="Org1MSP" PEER_ADDRESS="blockchain-org1peer1:30110" MSP_CONFIGPATH="/shared/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" create/join_channel.sh
+
+echo
+echo "Peer1 Org2 joining the channel"
 CHANNEL_NAME="labchannel" PEER_MSPID="Org2MSP" PEER_ADDRESS="blockchain-org2peer1:30210" MSP_CONFIGPATH="/shared/crypto-config/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp" create/join_channel.sh
